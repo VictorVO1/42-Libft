c20dc48aa4a301091eb3e813fb3a1283e896c5cc
Victor <victor-vo@hotmail.com>
2026-05-04 22:21:30 +0200

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,28 +12,83 @@
 
 #include "libft.h"
 
+int	words_in_string(char const *s, char c)
+{	
+	int	i;
+	int	words;
+
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			words++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (words);
+}
+
+int	word_len(char const *s, int start, char c)
+{
+	int	len;
+
+	len = 0;
+	while (s[start] && s[start] != c)
+	{
+		start++;
+		len++;
+	}
+	return (len);
+}
+
+void	free_all(char **arr, int count)
+{
+	int	i = 0;
+
+	while (i < count)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	/*char	**split;
+	char	**split;
 	size_t	i;
 	size_t	j;
 	size_t	len;
 
 	if (!s)
 		return (NULL);
-	len = ft_strlen(s);
+	split = malloc(sizeof(char *) * (words_in_string(s, c) + 1));
+	if (!split)
+        return (NULL);
 	i = 0;
-	while (s[i] && i < len)
+	j = 0;
+	while (s[i])
 	{
 		if (s[i] != c)
 		{
-			j = 0;
-			while (s[i + j] && s[i + j] != c)
-				j++;
-			split[0] = malloc(j - i + 1);
+			len = word_len(s, i, c);
+			split[j] = ft_substr(s, i, len);
+			if (!split[j])
+			{
+				free_all(split, j);
+                return (NULL);
+			}
+			j++;
+			i += len;
 		}
-		if (s[i] == c)
+		else
 			i++;
 	}
-	return (split);*/
+	split[j] = NULL;
+	return (split);
 }
